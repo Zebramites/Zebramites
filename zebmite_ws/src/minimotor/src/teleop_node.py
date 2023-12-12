13dace8d1aa8208b20ce08ead2eaad391dc2a9cb
BenBean18 <beakerthespeaker@gmail.com>
2023-12-11 21:24:42 -0500

    add intaking

--- zebmite_ws/src/minimotor/src/teleop_node.py
+++ zebmite_ws/src/minimotor/src/teleop_node.py
@@ -8,6 +8,7 @@ from std_msgs.msg import Float64
 rospy.init_node('teleop_node', anonymous=True)
 
 pub = rospy.Publisher("/minibot/mecanum_drive_controller/cmd_vel", Twist)
+pub_intake = rospy.Publisher("/minibot/intake_controller/command", Float64)
 
 def callback(msg):
     # publish cmd velocity with joystick input
@@ -15,6 +16,15 @@ def callback(msg):
     twist.linear.x = msg.axes[0]
     twist.linear.y = msg.axes[1]
     twist.angular.z = msg.axes[3]
+
+    # button 4 is intake, button 5 is outtake
+    if msg.buttons[4] == 1:
+        pub_intake.publish(1)
+    elif msg.buttons[5] == 1:
+        pub_intake.publish(-1)
+    else:
+        pub_intake.publish(0)
+    
     pub.publish(twist)
 
 
