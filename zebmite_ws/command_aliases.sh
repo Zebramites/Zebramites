445e14975f3b090d7db5b16036679a2510928cc6
BenBean18 <beakerthespeaker@gmail.com>
2024-04-26 22:47:39 -0400

    steal 900 docker and it works

--- zebmite_ws/command_aliases.sh
+++ zebmite_ws/command_aliases.sh
@@ -1,12 +1,12 @@
 # Aliases are short commands to replace long ones.
 
-alias crossbuild='~/minifrc/2023Zebramites/zebmite_ws/cross_build.sh'
+alias crossbuild='~/minifrc/Zebramites/zebmite_ws/cross_build.sh'
 alias deploy='~/scripts/deploy.sh'
 alias hwlaunch='roslaunch minimotor minibot_hardware.launch'
-alias killros='~/minifrc/2023Zebramites/zebmite_ws/kill_ros_.sh'
-alias natbuild='~/minifrc/2023Zebramites/zebmite_ws/native_build.sh'
-alias rosjet='source ~/minifrc/2023Zebramites/zebmite_ws/ROSJetsonMaster.sh'
-alias rosstd='source ~/minifrc/2023Zebramites/zebmite_ws/ROSStandard.sh'
+alias killros='~/minifrc/Zebramites/zebmite_ws/kill_ros_.sh'
+alias natbuild='~/minifrc/Zebramites/zebmite_ws/native_build.sh'
+alias rosjet='source ~/minifrc/Zebramites/zebmite_ws/ROSJetsonMaster.sh'
+alias rosstd='source ~/minifrc/Zebramites/zebmite_ws/ROSStandard.sh'
 alias simlaunch='roslaunch controller_node 2023_compbot_combined.launch hw_or_sim:=sim joy_or_key:=key button_box:=false'
 alias ls='ls --color'
 alias driversim='rqt --standalone rqt_driver_station_sim'
