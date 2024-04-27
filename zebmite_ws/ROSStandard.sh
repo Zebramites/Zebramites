6ed0547db0e3ebd5235c79d1973a264db8bf0101
BenBean18 <beakerthespeaker@gmail.com>
2024-04-26 23:30:11 -0400

    GPU detector works for my camera

--- zebmite_ws/ROSStandard.sh
+++ zebmite_ws/ROSStandard.sh
@@ -2,7 +2,7 @@
 
 # Setup ROS for Local Development
 source /opt/ros/noetic/setup.bash
-source ~/minifrc/2023Zebramites/zebmite_ws/devel/setup.bash
+source ~/Zebramites/zebmite_ws/devel/setup.bash
 export ROS_MASTER_URI="http://$(hostname -I | awk 'BEGIN { FS="[ ]" } ; { print $1 }'):5802"
 export ROS_IP="$(hostname -I | awk 'BEGIN { FS="[ ]" } ; { print $1 }')"
 export ROSLAUNCH_SSH_UNKNOWN=1
