e8e4297c6b2c16168a5f7526fccd7bf046d4f01a
Torchtopher <torchtopher@gmail.com>
2024-07-28 15:49:27 -0400

    4 note auto lol

--- zebmite_ws/src/minimotor/src/drive_to_point.py
+++ zebmite_ws/src/minimotor/src/drive_to_point.py
@@ -7,23 +7,33 @@ from tf.transformations import euler_from_quaternion
 
 from geometry_msgs.msg import Twist
 from tf2_geometry_msgs import PoseStamped
+import actionlib
+from minimotor_msgs.msg import DriveToPointAction, DriveToPointGoal, DriveToPointResult, DriveToPointFeedback
 
 class DriveToPoint:
-    def __init__(self):
+
+    _feedback = DriveToPointFeedback()
+    _result = DriveToPointResult()
+
+    def __init__(self, name):
+        self._action_name = name
+        self._as = actionlib.SimpleActionServer(self._action_name, DriveToPointAction, execute_cb=self.execute_cb, auto_start = False)
+        self._as.start()
+
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
         self.cmd_pub = rospy.Publisher('/minibot/mecanum_drive_controller/cmd_vel', Twist, queue_size=10)
-        self.goal_sub = rospy.Subscriber('/goal', PoseStamped, self.goal_callback)
         self.floor_frame = "tag_19"
-        self.robot_frame = "tag_9"
-
-        self.goal = None
+        self.robot_frame = "minifrc_robot"
         
-        self.MINIMUM_OUTPUT = 0.0
-        self.MAXIMUM_OUTPUT = 0.4
-        self.kP = 1.0
-        self.tolerance = 0.02
+        self.MINIMUM_OUTPUT_X = 0.1
+        self.MAXIMUM_OUTPUT_X = 1.0
+        self.MINIMUM_OUTPUT_Y = 0.1
+        self.MAXIMUM_OUTPUT_Y = 1.0
+        self.kP_X = 2.5
+        self.kP_Y = 1.8
+        self.default_tolerance = 0.02
 
     def goal_callback(self, msg: PoseStamped):
         rospy.loginfo("Received goal to drive to point")
@@ -33,19 +43,36 @@ class DriveToPoint:
         self.goal = goal
         rospy.loginfo(f"Driving to point: {goal.pose.position.x}, {goal.pose.position.y}")
 
-    def clamp(self, output):
+    def clamp(self, output, minimum_output, maximum_output):
         unsigned_output = output
-        if abs(output) > self.MAXIMUM_OUTPUT:
-            unsigned_output = self.MAXIMUM_OUTPUT
-        elif abs(output) < self.MINIMUM_OUTPUT:
-            unsigned_output = self.MINIMUM_OUTPUT
+        if abs(output) > maximum_output:
+            unsigned_output = maximum_output
+        elif abs(output) < minimum_output:
+            unsigned_output = minimum_output
         else:
             unsigned_output = abs(output)
         return unsigned_output * (1 if output > 0 else -1)
     
-    def main_loop(self):
+    def execute_cb(self, goal):
+        self.goal = goal.pose
+        if goal.tolerance > 0:
+            self.tolerance = goal.tolerance
+        else:
+            self.tolerance = self.default_tolerance
         r = rospy.Rate(50)
         while not rospy.is_shutdown():
+
+            if self._as.is_preempt_requested():
+                rospy.loginfo('%s: Preempted' % self._action_name)
+                twist = Twist()
+                twist.linear.x = 0.0
+                twist.linear.y = 0.0
+                twist.angular.z = 0.0
+                self.cmd_pub.publish(twist)
+                self._result.success = False
+                self._as.set_preempted(self._result)
+                return
+
             try:
                 current_pos = self.tf_buffer.lookup_transform(self.floor_frame, self.robot_frame, rospy.Time())
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
@@ -54,13 +81,8 @@ class DriveToPoint:
             
             x = current_pos.transform.translation.x
             y = current_pos.transform.translation.y
-            
-            if self.goal is None:
-                r.sleep()
-                continue
 
             if abs(x - self.goal.pose.position.x) < self.tolerance and abs(y - self.goal.pose.position.y) < self.tolerance:
-                self.goal = None
                 rospy.loginfo("Reached goal - Done")
                 # publish 0 twist
                 twist = Twist()
@@ -68,27 +90,29 @@ class DriveToPoint:
                 twist.linear.y = 0.0
                 twist.angular.z = 0.0
                 self.cmd_pub.publish(twist)
-                
-                # TODO make this an actionlib server
-            
-            if self.goal is None:
-                r.sleep()
-                continue
+
+                self._result.success = True
+                self._as.set_succeeded(self._result)
+                return
 
             twist = Twist()
-            x_command = self.kP * (self.goal.pose.position.x - x)
+            x_command = self.kP_X * (self.goal.pose.position.x - x)
             # twist.linear.x *= abs(x - self.goal.pose.position.x) > self.tolerance
-            y_command = self.kP * (self.goal.pose.position.y - y)
+            y_command = self.kP_Y * (self.goal.pose.position.y - y)
             # twist.linear.y *= abs(y - self.goal.pose.position.y) > self.tolerance
             rotate_angle = math.pi - euler_from_quaternion([current_pos.transform.rotation.x, current_pos.transform.rotation.y, current_pos.transform.rotation.z, current_pos.transform.rotation.w])[2]
             twist.linear.x = x_command * math.cos(rotate_angle) - y_command * math.sin(rotate_angle)
             twist.linear.y = x_command * math.sin(rotate_angle) + y_command * math.cos(rotate_angle)
             twist.angular.z = 0.0 #self.kP * rotate_angle
-            twist.linear.x = -self.clamp(twist.linear.x)
-            twist.linear.y = self.clamp(twist.linear.y)
+            twist.linear.x = -self.clamp(twist.linear.x, self.MINIMUM_OUTPUT_X, self.MAXIMUM_OUTPUT_X) * (abs(x - self.goal.pose.position.x) >= self.tolerance)
+            twist.linear.y = self.clamp(twist.linear.y, self.MINIMUM_OUTPUT_Y, self.MAXIMUM_OUTPUT_Y) * (abs(y - self.goal.pose.position.y) >= self.tolerance)
             rospy.loginfo_throttle(0.5, f"driving to point: {self.goal.pose.position.x}, {self.goal.pose.position.y}, current position: ({x}, {y}) @ {rotate_angle}, error = {(self.goal.pose.position.x - x)}, {(self.goal.pose.position.y - y)}, cmd_vel = {twist.linear.x}, {twist.linear.y}")
+            self._feedback.x_error = self.goal.pose.position.x - x
+            self._feedback.y_error = self.goal.pose.position.y - y
+            self._as.publish_feedback(self._feedback)
             self.cmd_pub.publish(twist)
             r.sleep()
+        
         twist = Twist()
         twist.linear.x = 0.0
         twist.linear.y = 0.0
@@ -97,5 +121,5 @@ class DriveToPoint:
     
 if __name__ == '__main__':
     rospy.init_node('drive_to_point')
-    dtp = DriveToPoint()
-    dtp.main_loop()
+    server = DriveToPoint("/minibot/drive_to_point")
+    rospy.spin()
---
+++ zebmite_ws/src/minimotor/src/four_note_auto.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# export ROS_MASTER_URI=http://10.32.4.124:5802
+import rospy
+import tf2_ros
+import math
+from tf.transformations import euler_from_quaternion
+import actionlib
+from geometry_msgs.msg import Twist
+from tf2_geometry_msgs import PoseStamped
+from geometry_msgs.msg import Point, Pose, Quaternion
+from sensor_msgs.msg import Joy
+from minimotor_msgs.msg import DriveToPointAction, DriveToPointGoal, DriveToPointResult, DriveToPointFeedback
+
+# "/goal" with geometry_msgs/PoseStamped, frame_id = "tag_19"
+
+# In front of left note
+'''
+buntu@dino-laptop:~$ rostopic pub /goal geometry_msgs/PoseStamped "header:
+    seq: 0
+    stamp:
+        secs: 0
+        nsecs: 0
+    frame_id: 'tag_19'
+pose:
+    position:
+        x: -0.38
+        y: -0.25
+        z: 0.0
+    orientation:
+        x: 0.0
+        y: 0.0
+        z: 0.0'''
+
+# "home"
+'''
+    seq: 0
+    stamp:
+        secs: 0
+        nsecs: 0
+    frame_id: 'tag_19'
+pose:
+    position:
+        x: 0.0
+        y: -0.1
+        z: 0.0
+    orientation:
+        x: 0.0
+        y: 0.0
+        z: 0.0
+        w: 0.0" -1''' 
+
+points = [[0.0, -0.4], [0.0, -0.1], [-0.38, -0.25], [-0.38, -0.4], [0.0, -0.1], [0.35, -0.25], [0.35, -0.4], [0.0, -0.1]]
+
+ac = actionlib.SimpleActionClient("/minibot/drive_to_point", DriveToPointAction)
+
+def joy_cb(msg: Joy):
+    if not msg.buttons[-3]:
+        return
+
+    pose_stamps = [PoseStamped(pose=Pose(position=Point(x=point[0], y=point[1], z=0), orientation=Quaternion(x=0, y=0, z=0, w=1))) for point in points]
+
+    for pose_stamped in pose_stamps:
+        goal = DriveToPointGoal()
+        rospy.loginfo(f"Driving to point {pose_stamped}")
+        goal.pose = pose_stamped
+        goal.tolerance = 0.02
+        done = False
+        def done_callback(state, result):
+            nonlocal done
+            rospy.loginfo(f"Drive to object actionlib finished with state {state} and result {result}")
+            done = True
+
+        r = rospy.Rate(50)
+        ac.send_goal(goal, done_cb=done_callback)
+
+        while not done and not rospy.is_shutdown():
+            r.sleep()
+
+#rospy.Subscriber("/minibot/joy_operator", Joy, operator_callback)
+# rospy.Subscriber("/minibot/joy", Joy, joy_cb)
+rospy.init_node('auto_node', anonymous=True)
+
+buttons = [1,1,1,1,1,1,1,1,1]
+msg = Joy(buttons=buttons)
+joy_cb(msg)
+
+# rospy.spin()
